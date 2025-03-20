560c8d0d835dfe41ff0183b5775aa45d6468e735
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-03-20 11:14:03 +0700

    Auto clear setelah LoadData

--- Form1.cs
+++ Form1.cs
@@ -45,6 +45,13 @@ namespace CRUD_Sederhana
                     conn.Open();
                     string query = "SELECT NIM AS [NIM], Nama, Email, Telepon, Alamat FROM Mahasiswa";
                     SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    dgvMahasiswa.AutoGenerateColumns = true;
+                    dgvMahasiswa.DataSource = dt;
+
+                    ClearForm(); //Auto clear setelah LoadData
                 }
             }
 
